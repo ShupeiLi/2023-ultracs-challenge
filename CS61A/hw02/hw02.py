526ca38419c8aa8164f7d44b822f960fa2813720
ShupeiLi <shupei.tina.li@gmail.com>
2023-01-10 21:43:30 +0100

    CS61A: 9/2 HW02

--- CS61A/hw02/hw02.py
+++ CS61A/hw02/hw02.py
@@ -31,7 +31,11 @@ def product(n, term):
     >>> product(3, triple)    # 1*3 * 2*3 * 3*3
     162
     """
-    "*** YOUR CODE HERE ***"
+    # "*** YOUR CODE HERE ***"
+    total = 1
+    for i in range(1, 1 + n):
+        total *= term(i)
+    return total
 
 
 def accumulate(merger, start, n, term):
@@ -58,7 +62,11 @@ def accumulate(merger, start, n, term):
     >>> accumulate(lambda x, y: (x + y) % 17, 19, 20, square)
     16
     """
-    "*** YOUR CODE HERE ***"
+    # "*** YOUR CODE HERE ***"
+    total = start
+    for i in range(1, n + 1):
+        total = merger(total, term(i))
+    return total
 
 
 def summation_using_accumulate(n, term):
@@ -75,7 +83,7 @@ def summation_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    "*** YOUR CODE HERE ***"
+    return accumulate(add, 0, n, term)
 
 
 def product_using_accumulate(n, term):
@@ -92,4 +100,4 @@ def product_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    "*** YOUR CODE HERE ***"
+    return accumulate(mul, 1, n, term)
